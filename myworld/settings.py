8ea1af870c19e6e4008e32b1ff2ed3c6fd31ae4e
niyokwizer <niyokwizerwavayire@gmail.com>
2022-08-17 11:08:34 -0700

    update

--- myworld/settings.py
+++ myworld/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from email.mime import image
 from pathlib import Path
 import os .path
-import django_heroku
+#import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -146,4 +146,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL = 'home'
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
